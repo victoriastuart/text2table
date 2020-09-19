7f0d42a90668d5bc1c6b1efb855d7acbaf3cfe02
Victoria Stuart <victoriastuart@users.noreply.github.com>
2020-09-19 00:21:25 -0700

    Update text2table.sh

--- text2table.sh
+++ text2table.sh
@@ -6,7 +6,7 @@ export LANG=C.UTF-8
 #          file: /mnt/Vancouver/programming/scripts/text2table.sh
 #       created: 2020-09-04
 #       version: 02
-# last modified: 2020-09-18 15:10:11 -0700 (PST)
+# last modified: 2020-09-19 00:18:26 -0700 (PST)
 # =============================================================================
 
 ## Remove old tmp file (if exists):
@@ -21,8 +21,8 @@ touch /tmp/t2t_table
 
 ## Usage: ttt <file>
 
-printf 'Usage: /mnt/Vancouver/programming/scripts/text2table.sh input (file or dir)\n'
-
+# printf 'Usage (example): /mnt/Vancouver/programming/scripts/text2table.sh /tmp/table\n'
+printf 'Usage: ttt <file name>\n'
 
 # ----------------------------------------------------------------------------
 ## Process |-delimited input file
@@ -54,64 +54,26 @@ sed -i '3s/^/\t<tfoot><tr><td colspan="4"><font size="-1">This is the footer and
 
 echo
 
+# ----------------------------------------------------------------------------
 <<COMMENT
-==============================================================================
-text2table.sh
-2020-09-04
 
 <table style="table-layout:fixed; width:auto;">
 <!-- Change width to 100% to be width of page [https://www.thesitewizard.com/css/make-table-cells-same-size.shtml]. -->
 
 ----------------------------------------
-CELL ALIGNMENT
-How to center align text in table cells in HTML?
+Cell alignment: aligning text in table cells
+https://www.tutorialspoint.com/How-to-center-align-text-in-table-cells-in-HTML
 
   <td style="text-align:left|center|right">table cell</td>
 
 e.g.
 
-  <td style="text-align:center">MySQL</td>
+  <td style="text-align:center">Apples</td>
 
-https://www.tutorialspoint.com/How-to-center-align-text-in-table-cells-in-HTML
 ----------------------------------------
+<!-- Title row with light blue (Wikipedia) background: -->
 
-<!-- Title row, NICE background (light blue): -->
 <tbody><tr style="background:#b0c4de;">
 
-<table>
-    <thead><tr><td colspan="5">This is the header and should span all columns.</td></tr></thead>
-    <tfoot><tr><td colspan="5">This is the footer and should span all columns.</td></tr></tfoot>
-    <tbody>
-        <tr><td>11</td><td>12</td><td>13</td><td>14</td><td>15</td>
-        <tr><td>21</td><td>22</td><td>23</td><td>24</td><td>25</td>
-        <tr><td>31</td><td>32</td><td>33</td><td>34</td><td>35</td>
-    </tbody>
-</table>
-
-<table>
-  <tr>
-    <td><b>Col 1 Header:</b></td>
-    <td><b></center>Col 2 Header:<sup><b><font color=red>*</font></b></sup></b></td>
-  </tr>
-  <tr>
-    <td><code><font size="2">Row 1 Col 1</font></code></td>
-    <td><code><font size="2">Row 1 Col 2</font></code></td>
-    <td><code><font size="2">Row 1 Col 3</font></code></td>
-  </tr>
-  <tr>
-    <td><code><font size="2">Row 2 Col 1</font></code></td>
-    <td><code><font size="2">Row 2 Col 2</font></code></td>
-    <td><code><font size="2">Row 2 Col 3</font></code></td>
-  </tr>
-  <tr>
-    <td><code><font size="2">Row 3 Col 1</font></code></td>
-    <td><code><font size="2">Row 3 Col 2</font></code></td>
-    <td><code><font size="2">Row 3 Col 3</font></code></td>
-  </tr>
-    <td colspan=2><sup><b><font color=red>*</font></b></sup><code><font size="2">col span - footnote -  line 1<br>&nbsp;col span - footnote -  line 2</font></code></td>
-  <tr>
-  </tr>
-</table>
-==============================================================================
 COMMENT
-
+# ----------------------------------------------------------------------------
