25a0fe5c76dc9e0f83ce00a477d58b4c2c63c7fa
Victoria Stuart <victoriastuart@users.noreply.github.com>
2021-07-16 20:22:00 -0700

    Update text2table.sh

--- text2table.sh
+++ text2table.sh
@@ -3,10 +3,10 @@
 # vim: syntax=sh autoindent tabstop=4 shiftwidth=4 expandtab softtabstop=4 textwidth=220
 export LANG=C.UTF-8
 
-#          file: /mnt/Vancouver/programming/scripts/text2table.sh
+#          file: text2table.sh
 #       created: 2020-09-04
-#       version: 02
-# last modified: 2020-09-19 00:18:26 -0700 (PST)
+#       version: 03
+# last modified: 2021-07-16
 # =============================================================================
 
 ## Remove old tmp file (if exists):
@@ -21,37 +21,29 @@ touch /tmp/t2t_table
 
 ## Usage: ttt <file>
 
-# printf 'Usage (example): /mnt/Vancouver/programming/scripts/text2table.sh /tmp/table\n'
 printf 'Usage: ttt <file name>\n'
 
 # ----------------------------------------------------------------------------
 ## Process |-delimited input file
 
 sed -i 's/|/<\/td><td>/g' $1
-sed -i 's/^/\t<tr><td>/g' $1
-sed -i 's/$/<\/td>/g' $1
+sed -i 's/^/\t\t<tr><td>/g' $1
+sed -i 's/$/<\/td><\/tr>\n/g' $1
 
 # ----------------------------------------------------------------------------
 ## TABLE HEADERS, FOOTERS:
 
-## Do this first; add blank line, or sed 1s will overwrite first line
-# printf '\n\t</tbody>\n</table>\n' >> /tmp/t2t_table
-
 printf '\t</tbody>\n</table>' >> $1
 
-## https://stackoverflow.com/questions/9533679/how-to-insert-a-text-at-the-beginning-of-a-file
-## sed won't work on empty file, so do the printf command (above), first.
+sed -i '1s/^/\n\t<tbody>\n\n/' $1
 
-# sed -i '1s/^/<table">\n/' /tmp/t2t_table
-# sed -i '2s/^/\t<thead><tr><td colspan="4">This is the header and should span all columns.<\/td><\/tr><\/thead>\n/' /tmp/t2t_table
-# sed -i '3s/^/\t<tfoot><tr><td colspan="4">This is the footer and should span all columns.<\/td><\/tr><\/thead>\n/' /tmp/t2t_table
-# cat /tmp/t2t_table
+sed -i '1s/^/\n\t<tfoot><tr><td colspan="4">This is the footer and should span all columns.<\/td><\/tr><\/tfoot>\n/' $1
+
+sed -i '1s/^/\n\t<thead><tr style="background:#b0c4de;"><td colspan="4"><b>This is the header and should span all columns.<\/b><\/td><\/tr><\/thead>\n/' $1
 
 sed -i '1s/^/<table style="table-layout:fixed; width:auto;">\n/' $1
-sed -i '2s/^/\t<thead><tr style="background:#b0c4de;"><td colspan="4"><b>This is the header and should span all columns.<\/b><\/td><\/tr><\/thead>\n/' $1
-sed -i '3s/^/\t<tfoot><tr><td colspan="4"><font size="-1">This is the footer and should span all columns.<\/font><\/td><\/tr><\/tfoot>\n/' $1
-# echo 'Done!'
 
+# echo 'Done!'
 echo
 
 # ----------------------------------------------------------------------------
